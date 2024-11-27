32afa93179076a581295ba81a7fe1f72968ba8ee
erik <erwarnkeva@est.utn.ac.cr>
2024-11-27 13:51:44 -0600

    DTOParticipante

---
+++ Model/DTO/ParticipanteDTO.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.DTO;
+
+/**
+ *
+ * @author Student
+ */
+public class ParticipanteDTO {
+   private final String cedula;
+    private final String nombre; 
+    private final int telefono; 
+    private final String correo; 
+
+    public ParticipanteDTO(String cedula, String nombre, int telefono, String correo) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+}
