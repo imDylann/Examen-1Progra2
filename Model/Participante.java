01f41d9da2e700b4305d23db215297e69559e43c
erik <erwarnkeva@est.utn.ac.cr>
2024-11-27 13:41:24 -0600

    pequeño change

--- Model/Participante.java
+++ Model/Participante.java
@@ -1,3 +1,5 @@
+package Model;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -42,6 +44,11 @@ public class Participante {
 
     public void setCorreo(String correo) {
         this.correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return "Participante{" + "cedula=" + cedula + ", nombre=" + nombre + ", telefono=" + telefono + ", correo=" + correo + '}';
     }
     
     
